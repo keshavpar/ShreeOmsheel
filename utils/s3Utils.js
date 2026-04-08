74eb56ddb862683e1c9d3d8ae4af55d3b9803a07
keshavpar <keshavparihar54@gmail.com>
2026-04-08 21:27:52 +0530

    updated S3utils

--- utils/s3Utils.js
+++ utils/s3Utils.js
@@ -1,5 +1,14 @@
-const AWS = require('aws-sdk');
+
 const s3 = new AWS.S3();
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
 
 const getSignedUrl = (key, expiresInSeconds = 300) => {
   if (!key) return null; // handle empty keys gracefully
